352c606bc75a8af034627e270432e6a36a721123
JKEugene <129939029+JKEugene@users.noreply.github.com>
2025-10-19 13:05:14 +0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,25 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const burger = document.querySelector('.burger');
-    const navLinks = document.querySelector('.nav-links');
+const header = document.querySelector('header');
+const burger = document.querySelector('.burger');
+const navLinks = document.querySelector('.nav-links');
 
+document.addEventListener('DOMContentLoaded', function() {
     burger.addEventListener('click', function() {
         navLinks.classList.toggle('open');
         burger.classList.toggle('active');
     });
+});
+
+let lastScrollTop = 0;
+window.addEventListener('scroll', () => {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    if (scrollTop > lastScrollTop) {
+        if (navLinks.classList.contains('open')) {
+            navLinks.classList.remove('open');
+            burger.classList.remove('active');
+        }
+        header.style.top = `-${header.offsetHeight}px`;
+    } else {
+        header.style.top = '0';
+    }
+    lastScrollTop = scrollTop;
 });
