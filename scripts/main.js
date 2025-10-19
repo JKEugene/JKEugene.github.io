3765d1686e4097632f377faaafb95944c2e54d4d
JKEugene <129939029+JKEugene@users.noreply.github.com>
2025-10-19 14:40:47 +0500

    Enhance navigation with close button functionality
    Added functionality to close navigation menu.

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,20 @@
 const header = document.querySelector('header');
 const burger = document.querySelector('.burger');
 const navLinks = document.querySelector('.nav-links');
+const closeNav = document.querySelector('.close-nav');
 
 document.addEventListener('DOMContentLoaded', function() {
     burger.addEventListener('click', function() {
         navLinks.classList.toggle('open');
         burger.classList.toggle('active');
+        closeNav.style.display = navLinks.classList.contains('open') ? 'block' : 'none';
     });
+});
+
+closeNav.addEventListener('click', function() {
+    navLinks.classList.remove('open');
+    burger.classList.remove('active');
+    closeNav.style.display = 'none';
 });
 
 let lastScrollTop = 0;
@@ -16,6 +24,7 @@ window.addEventListener('scroll', () => {
         if (navLinks.classList.contains('open')) {
             navLinks.classList.remove('open');
             burger.classList.remove('active');
+            closeNav.style.display = 'none';
         }
         header.style.top = `-${header.offsetHeight}px`;
     } else {
